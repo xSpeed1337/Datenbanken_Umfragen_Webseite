96baf6a1927be0f12e65a5f728960c87fc638477
Lukas Fink <speedcrafter1@gmail.com>
2020-04-08 13:25:40 +0200

    Created schema in create_tables.sql

--- SQL/create_tables.sql
+++ SQL/create_tables.sql
@@ -1,9 +1,15 @@
+DROP SCHEMA IF EXISTS create_database;
+CREATE SCHEMA IF NOT EXISTS create_database DEFAULT CHARACTER SET utf8;
+USE create_database;
+
+DROP TABLE IF EXISTS surveyor;
 CREATE TABLE surveyor
 (
     username VARCHAR(32) PRIMARY KEY UNIQUE,
     password VARCHAR(32) NOT NULL
 );
 
+DROP TABLE IF EXISTS survey;
 CREATE TABLE survey
 (
     title_short CHAR(5) PRIMARY KEY UNIQUE,
@@ -12,6 +18,7 @@ CREATE TABLE survey
     FOREIGN KEY (username) REFERENCES surveyor (username)
 );
 
+DROP TABLE IF EXISTS question;
 CREATE TABLE question
 (
     id            INT PRIMARY KEY AUTO_INCREMENT,
@@ -20,21 +27,24 @@ CREATE TABLE question
     FOREIGN KEY (title_short) REFERENCES survey (title_short)
 );
 
+DROP TABLE IF EXISTS course;
 CREATE TABLE course
 (
     course_short VARCHAR(5) PRIMARY KEY,
     course_name  VARCHAR(64) NOT NULL
 );
 
+DROP TABLE IF EXISTS student;
 CREATE TABLE student
 (
-    matnr        INT(8) PRIMARY KEY UNIQUE,
+    matnr        INT(7) PRIMARY KEY UNIQUE,
     firstname    VARCHAR(32) NOT NULL,
     lastname     VARCHAR(32) NOT NULL,
     course_short VARCHAR(5),
     FOREIGN KEY (course_short) REFERENCES course (course_short)
 );
 
+DROP TABLE IF EXISTS assigned_course;
 CREATE TABLE assigned_course
 (
     title_short  CHAR(5),
@@ -44,20 +54,22 @@ CREATE TABLE assigned_course
     PRIMARY KEY (title_short, course_short)
 );
 
+DROP TABLE IF EXISTS answer;
 CREATE TABLE answer
 (
-    id          INT,
-    matnr       INT(8),
-    answer_text VARCHAR(500),
+    id     INT,
+    matnr  INT(7),
+    answer INT(1),
     FOREIGN KEY (id) references question (id),
     FOREIGN KEY (matnr) references student (matnr),
     PRIMARY KEY (id, matnr)
 );
 
+DROP TABLE IF EXISTS finished;
 CREATE TABLE finished
 (
     title_short CHAR(5),
-    matnr       INT(8),
+    matnr       INT(7),
     FOREIGN KEY (title_short) references survey (title_short),
     FOREIGN KEY (matnr) references student (matnr),
     PRIMARY KEY (title_short, matnr)
