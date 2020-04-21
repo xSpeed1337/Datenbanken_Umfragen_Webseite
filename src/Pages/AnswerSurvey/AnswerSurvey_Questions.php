474360c8f63e3e2b178d4de28b7dc53c275aa4b1
DeckertE <elenadeckert@web.de>
2020-04-21 10:35:23 +0200

    Pages created

--- src/Pages/AnswerSurvey/AnswerSurvey_Questions.php
+++ src/Pages/AnswerSurvey/AnswerSurvey_Questions.php
@@ -1,3 +1,8 @@
+<?php session_start();
+include_once "../../php-scripts/DatabaseHandler.php";
+include_once "../../php-scripts/SurveyAnswer.php";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,11 +12,34 @@
 </head>
 <body>
 
-<div>
+<?php
 
-    <h2>Evaluation der Vorlesung "Einführung in die BWL"</h2>
+if ((isset($_POST["PrevQuestion"]) == false) && (isset($_POST["NextQuestion"]) == false)) {
 
-    <form method="POST"/>
+    $_SESSION["currentSite"] = 1;
+    //Mit Post-Variable Fragebogen Titel zuordnen bei Klick auf Fragebogen beantworten
+    $_SESSION["FB_title_short"] = "TEST";
+
+    $_SESSION["NumberOfQuestions"] = getAnzQuestions($_SESSION["FB_title_short"]);
+
+
+} elseif(isset($_POST["PrevQuestion"]) == true) {
+    $_SESSION["currentSite"] ++;
+
+    }elseif(isset($_POST["NextQuestion"]) == true) {
+        $_SESSION ["currentSite"] --;
+    }
+
+?>
+
+<h2>Evaluation der Vorlesung "Einführung in die BWL"</h2>
+
+<?php
+//Fragen aufrufen
+?>
+
+
+    <form method="POST" action="AnswerSurvey_Questions.php"/>
     <table>
         <tr>
             <td style="padding-right:20px">Frage 1:Wurden die Inhalte der Vorlesung verständlich erklärt?</td>
@@ -26,10 +54,23 @@
                     5<input type="radio" name="Radio" value="5"/></td>
         </tr>
 
+
         <tr style="height:50px">
-            <td><button type="submit" name="PrevQuestion">Vorherige Frage</button>
-                <button type="submit" name="NextQuestion">Nächste Frage</button></td>
+            <td><button type="submit"
+                        <?php
+                        if($_SESSION ["CurrentSite"] == $_SESSION["NumberOfPages"])
+                            echo "disabled";
+                        ?>
+                        name="PrevQuestion">Vorherige Frage</button>
+
+                <button type="submit"
+                        <?php
+                        if($_SESSION["CurrentSite"] == 1)
+                            echo "disabled";
+                        ?>
+                        name="NextQuestion">Nächste Frage</button></td>
         </tr>
+
 
         <tr style="height:50px">
             <td><button type="submit" name="BackToHP">Zurück zum Hauptmenü</button></td>
@@ -37,8 +78,6 @@
 
     </table>
     </form>
-
-</div>
 
 
 </body>
---
+++ src/php-scripts/SurveyAnswer.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once "DatabaseHandler.php";
+
+
+class SurveyAnswerHandler extends DatabaseHandler{
+
+
+    public function getAnzQuestions($fb_short_title){
+
+        $sql = "SELECT COUNT(*) AS 'NUMBER_OF_QUESTIONS' FROM question WHERE title_short = '".$fb_short_title."'";
+        $stmt = $this->connect()->query($sql);
+        $row = $stmt->fetch();
+
+        if ($row > 0){
+            return $row["NUMBER_OF_QUESTIONS"];
+            echo $row ['NUMBER_OF_QUESTIONS'];
+        }else{
+            echo "Keine Fragen ermittelt!";
+        }
+
+    }
+
+}
+
+
