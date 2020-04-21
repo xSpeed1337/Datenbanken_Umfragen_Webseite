47b6855d1acbcac7349e06e7d81fa1a3693c681d
Lukas Fink <speedcrafter1@gmail.com>
2020-04-21 10:32:55 +0200

    Created test_data.sql and deleting tables

---
+++ src/SQL/test_data.sql
@@ -0,0 +1,18 @@
+DELETE
+FROM survey_assigned_course;
+DELETE
+FROM survey_commented;
+DELETE
+FROM survey_finished;
+DELETE
+FROM question_answer;
+DELETE
+FROM question;
+DELETE
+FROM student;
+DELETE
+FROM survey;
+DELETE
+FROM course;
+DELETE
+FROM surveyor;
