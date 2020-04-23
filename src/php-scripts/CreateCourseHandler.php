6dc75c95794fa9f40e4850ddddc0a0fe4e2da2cb
Lukas Fink <speedcrafter1@gmail.com>
2020-04-23 11:22:37 +0200

    Created CreateCourseHandler.php

---
+++ src/php-scripts/CreateCourseHandler.php
@@ -0,0 +1,15 @@
+<?php
+
+include_once "DatabaseHandler.php";
+
+class CreateCourseHandler extends DatabaseHandler {
+
+    public function createCourse() {
+
+    }
+
+    public function createStudents() {
+
+    }
+
+}
