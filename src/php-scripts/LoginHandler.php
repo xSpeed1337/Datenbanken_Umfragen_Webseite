4751e4fa12d3b3e120d6f4641c55ba82ee0d0445
Lukas Fink <speedcrafter1@gmail.com>
2020-04-24 08:46:53 +0200

    fixed LoginHandler.php

--- src/php-scripts/LoginHandler.php
+++ src/php-scripts/LoginHandler.php
@@ -4,22 +4,26 @@ require "utilities.php";
 class LoginHandler {
 
 
-    public function register(){
+    public function register() {
 
-        if(empty($_POST["username"]) || empty($_POST["password"]))
-        {
+        if (empty($_POST["username"]) || empty($_POST["password"])) {
             alert("Both fields are required");
-        }
-        else
-        {
+        } else {
             $username = $_POST["username"];
             $password = $_POST["password"];
-            $password = password_hash($password, PASSWORD_DEFAULT);
-            $query = "INSERT INTO surveyor(username, password) VALUES('$username', '$password')";
-            if(mysqli_query(database_connect(), $query))
-            {
-                alert("Registration successful");
 
+            $password = password_hash($password, PASSWORD_DEFAULT);
+            $sql = "INSERT INTO surveyor(username, password) VALUES(?, ?)";
+            $stmt = mysqli_stmt_init(database_connect());
+            if (!mysqli_stmt_prepare($stmt, $sql)) {
+                echo "SQL statement failed";
+            } else {
+                mysqli_stmt_bind_param($stmt, "ss", $username, $password);
+                if (mysqli_stmt_execute($stmt)) {
+                    alert("Registration successful");
+                } else {
+                    alert("Registration not successful");
+                };
             }
         }
     }
@@ -34,27 +38,24 @@ class LoginHandler {
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             echo "SQL statement failed";
         } else {
-            mysqli_stmt_bind_param($stmt, "ss", $_username);
+            mysqli_stmt_bind_param($stmt, "s", $username);
             mysqli_stmt_execute($stmt);
         }
 
         $result = $stmt->get_result();
 
-        if($result->num_rows > 0){
-            while($row = mysqli_fetch_array($result)){
-                if(password_verify($password, $row["password"])){
+        if ($result->num_rows > 0) {
+            while ($row = mysqli_fetch_array($result)) {
+                if (password_verify($password, $row["password"])) {
                     $_SESSION['username'] = $username;
                     header("Location: ../Pages/MySurveys_Interviewer.php");
-                }else{
+                } else {
                     alert("Wrong password");
                 }
             }
-        }else{
-
+        } else {
             alert("Username does not exist");
-
         }
-
     }
 
     public function loginStudent() {
@@ -82,15 +83,15 @@ class LoginHandler {
 
 }
 
-function alert($message){
+function alert($message) {
     echo "<script>alert('$message'); window.location.href='../Pages/login.php';</script>";
 }
 
 $h = new LoginHandler();
 
-if (isset($_POST["register"])){
+if (isset($_POST["register"])) {
     $h->register();
-}elseif(isset($_POST["loginInter"])) {
+} elseif (isset($_POST["loginInter"])) {
     $h->loginSurveyor();
 } elseif (isset($_POST["loginStudent"])) {
     $h->loginStudent();
