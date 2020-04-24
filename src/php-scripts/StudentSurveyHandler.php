f87f0124b4bc97b8e7d261368e1daa3360023269
DeckertE <elenadeckert@web.de>
2020-04-24 08:10:01 +0200

    Umstellung auf mysqli

--- src/php-scripts/StudentSurveyHandler.php
+++ src/php-scripts/StudentSurveyHandler.php
@@ -1,29 +1,36 @@
 <?php
-include_once "DatabaseHandler.php";
+include_once "utilities.php";
 
-class StudentSurveyHandler extends DatabaseHandler{
+class StudentSurveyHandler {
 
     /*Elena Deckert*/
     /*Generierung der Infos zu den Fragebögen, die einem Student zugeordnet sind (in MySurveys_Student.php)*/
 
     public function getSurveysStudent($matnr) {
 
-        $sql = "SELECT * FROM survey WHERE title_short IN 
-            ( SELECT surv.title_short FROM survey_assigned_course AS surv INNER JOIN student AS stud ON surv.course_short = stud.course_short WHERE stud.matnr ='".$matnr."')";
-        $stmt = $this->connect()->query($sql);
-        $surveys = $stmt->fetchAll();
+        $db = database_connect();
+        $cmd = mysqli_prepare($db,"SELECT * FROM survey WHERE title_short IN 
+            ( SELECT surv.title_short FROM survey_assigned_course AS surv INNER JOIN student AS stud ON surv.course_short = stud.course_short WHERE stud.matnr = ?)");
+        mysqli_stmt_bind_param($cmd, "i", $matnr);
+        mysqli_stmt_execute($cmd);
+        $surveys = mysqli_stmt_get_result($cmd);
         return $surveys;
+
     }
 
 
+    ////////////////////////////////////////////////////////////////
 
     /*Elena Deckert*/
     /*Fragen und FrageID zum ausgewählten Fragebogen ermitteln */
     public function getQuestions($fb_short_title){
 
-        $sql = "SELECT * FROM question WHERE title_short = '".$fb_short_title."'";
-        $stmt = $this->connect()->query($sql);
-        $results = $stmt->fetchAll();
+
+        $db = database_connect();
+        $cmd = mysqli_prepare($db,"SELECT * FROM question WHERE title_short = ? ");
+        mysqli_stmt_bind_param($cmd, "s", $fb_short_title);
+        mysqli_stmt_execute($cmd);
+        $results = mysqli_stmt_get_result($cmd);
 
         //$questions = array('$key' => "$question");
         $questions = [];
@@ -39,41 +46,50 @@ class StudentSurveyHandler extends DatabaseHandler{
         }else{
             echo "Keine Fragen ermittelt!";
         }
+
     }
 
-
+    ////////////////////////////////////////////////////////////////
 
     /*Elena Deckert*/
     /*Antworten der Studenten speichern bzw. updaten, wenn schon eine Antwort gewählt wurde*/
     public function saveAnswer($answer, $questionID, $matnr) {
 
-        $sql = "SELECT * FROM question_answer WHERE id ='".$questionID."' AND matnr ='".$matnr."'";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$questionID, $matnr]);
-        $results = $stmt->fetch();
+
+
+        $db = database_connect();
+        $cmd = mysqli_prepare($db,"SELECT * FROM question_answer WHERE id = ? AND matnr = ?");
+        mysqli_stmt_bind_param($cmd, "ii",$questionID, $matnr);
+        mysqli_stmt_execute($cmd);
+        $results = mysqli_stmt_get_result($cmd);
 
         if($results == false){
-            $sql = "INSERT INTO question_answer(id, matnr, answer) VALUES( '".$questionID."','".$matnr."','".$answer."')";
-            $stmt = $this->connect()->prepare($sql);
-            $stmt->execute();
-
-        }else{
-            $sql = "UPDATE question_answer SET answer ='".$answer."' WHERE id ='".$questionID."' AND matnr ='".$matnr."'";
-            $stmt = $this->connect()->prepare($sql);
-            $stmt->execute();
+            $cmd = mysqli_prepare($db,"INSERT INTO question_answer(id, matnr, answer) VALUES( ?, ?, ?)");
+            mysqli_stmt_bind_param($cmd, "iis",$questionID, $matnr, $answer);
+            mysqli_stmt_execute($cmd);
         }
+
+        else{
+            $cmd = mysqli_prepare($db,"UPDATE question_answer SET answer = ? WHERE id = ? AND matnr = ?");
+            mysqli_stmt_bind_param($cmd, "sii",$answer, $questionID, $matnr);
+            mysqli_stmt_execute($cmd);
+        }
+
     }
 
-
+    ////////////////////////////////////////////////////////////////
 
     /*Elena Deckert*/
     /*Vorbelegung der Radiobuttons, falls bereits eine Antwort in der Datenbank gespeichert ist*/
     public function getRadioButtons($questionID, $matnr){
 
-        $sql = "SELECT * FROM question_answer WHERE id ='".$questionID."' AND matnr ='".$matnr."'";
-        $stmt = $this->connect()->prepare($sql);
-        $stmt->execute([$questionID, $matnr]);
-        $results = $stmt->fetch();
+
+        $db = database_connect();
+        $cmd = mysqli_prepare($db,"SELECT * FROM question_answer WHERE id = ? AND matnr = ?");
+        mysqli_stmt_bind_param($cmd, "ii", $questionID,$matnr );
+        mysqli_stmt_execute($cmd);
+        $result = mysqli_stmt_get_result($cmd); //ganze Tabelle
+        $results = mysqli_fetch_assoc($result); //erster Eintrag
 
         if($results == false){
             echo
