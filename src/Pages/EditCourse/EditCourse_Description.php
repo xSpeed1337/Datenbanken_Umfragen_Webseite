4f209d4b3c0ed8cbca1c5d36d92e261c30dcb35b
Lukas Fink <speedcrafter1@gmail.com>
2020-04-28 14:49:26 +0200

    created EditCourse_Description.php

--- src/Pages/EditCourse/EditCourse_Description.php
+++ src/Pages/EditCourse/EditCourse_Description.php
@@ -1,1 +1,66 @@
 <?php
+require "../../php-scripts/Utilities.php";
+
+if (!isset($_SESSION['username'])) {
+    header('Location: ../LoginPage.php');
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kurs bearbeiten</title>
+</head>
+<body>
+<h2>Kurs bearbeiten</h2>
+<form method="post" action="../../php-scripts/CreateCourseHandler.php">
+    <table>
+
+        <tr>
+            <td>Zu bearbeitender Kurs:</td>
+            <td>
+                <select name="OldCourseShort">
+                    <?php
+                    $sql = "SELECT course_short FROM course";
+                    $stmt = mysqli_stmt_init(database_connect());
+                    if (!mysqli_stmt_prepare($stmt, $sql)) {
+                        echo "SQL statement failed";
+                    } else {
+                        mysqli_stmt_execute($stmt);
+                        $results = mysqli_stmt_get_result($stmt);
+
+                        foreach ($results as $course) {
+                            echo "<option value=\"" . $course['course_short'] . "\">" . $course['course_short'] . " " . $course['course_name'] . "</option>";
+                        }
+                    }
+                    ?>
+                </select>
+            </td>
+        </tr>
+
+        <tr>
+            <td>Neue Kurskurzbezeichnung:</td>
+            <td><input required type="text" maxlength="10" name="UpdateCourseDesc"/></td>
+        </tr>
+
+        <tr>
+            <td>Neue Kursname:</td>
+            <td><input required type="text" maxlength="64" name="UpdateCourseName"/></td>
+        </tr>
+
+        <tr style="height:50px">
+            <td>
+                <button type="submit" name="UpdateSave">Speichern</button>
+            </td>
+        </tr>
+
+    </table>
+</form>
+<form action="../MySurveys_Interviewer.php" method="GET">
+    <button type="submit" name="Quit">Abbrechen</button>
+</form>
+</body>
+</html>
